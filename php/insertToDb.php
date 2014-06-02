274228c6e393708b6ab763847d1b7412cdd79780
Narendran Sankaran <narendrans@users.noreply.github.com>
2014-06-02 14:04:32 -0400

    Update insertToDb.php

--- php/insertToDb.php
+++ php/insertToDb.php
@@ -1,25 +1,24 @@
 <?php
 /*
 
-    @author Naren
-    @version 1.0
-    
-    This script is used to push quotes into sql db. 
-    MUST only be called by the Java program.
+ @author Naren
+ @version 1.0
 
-*/
+ This script is used to push quotes into sql db.
+ MUST only be called by the Java program.
+
+ */
 
 // DB config
-$con=mysqli_connect("localhost","root","","quotes");
+$con = mysqli_connect("localhost", "root", "", "quotes");
 // Check connection
-if (mysqli_connect_errno())
-{
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if (mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
 // insert data to db
-mysqli_query($con,"INSERT INTO quote (title, quote)
-VALUES ('".$_POST['title']."', '".$_POST['quote']."')");
+mysqli_query($con, "INSERT INTO quote (title, quote)
+VALUES ('" . $_POST['title'] . "', '" . $_POST['quote'] . "')");
 
 // post message if success
 echo "success";
